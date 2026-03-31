8d4aad43f16571827e11ed4c3c8e4d57a7698faa
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-31 21:29:17 +0800

    update

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from openai import OpenAI
 import os
 
-
 client = OpenAI(
 	api_key=os.getenv("OPENAI_API_KEY"),
 	base_url=os.getenv("OPENAI_BASE_URL")
@@ -10,9 +9,12 @@ client = OpenAI(
 completion = client.chat.completions.create(
 	model="kimi-k2.5",  
 	messages=[{'role': 'system', 'content': 'You are a helpful assistant.'},
-			  {'role': 'user', 'content': 'If 2x+3y=10, 3x-4y=-2; then what is x and y?'}]
-	)
+			  {'role': 'user', 'content': 'If 2x+3y=10, 3x-4y=-2; then what is x and y?'}],
+    max_completion_tokens=1000
+)
+
 print("="*50+"completion as json"+"="*50)
 print(completion.model_dump_json())
+
 print("="*55+"response"+"="*55)
 print(completion.choices[0].message.content)
